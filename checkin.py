9da11a2063ac18954c2aabd5ca49285c8ac4fdb3
Dongsheng Cai <d@tux.im>
2015-10-30 17:23:51 +1100

    Add v2ex checkin

--- checkin.py
+++ checkin.py
@@ -6,6 +6,8 @@ import requests
 import time
 import configparser
 import os,sys
+from bs4 import BeautifulSoup
+
 try:
     import http.client as http_client
 except ImportError:
@@ -15,6 +17,7 @@ except ImportError:
 cfg = configparser.ConfigParser()
 dirname=os.path.dirname(os.path.realpath(sys.argv[0]))
 cfg.read("{}/credentials.ini".format(dirname))
+
 
 def checkin_netease(music_u, csrf):
     TYPE_WEBPC = 1
@@ -72,6 +75,43 @@ def checkin_zimuzu(username, password):
     o = r3.json()
     logging.info(r3.content)
 
+def checkin_v2ex(username, password):
+    login_url = 'https://v2ex.com/signin'
+    home_page = 'https://www.v2ex.com'
+    mission_url = 'https://www.v2ex.com/mission/daily'
+    UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
+    headers = {
+        'User-Agent' : UA,
+        'Host' : "www.v2ex.com",
+        'Referer' : 'https://www.v2ex.com/signin',
+        'Origin' : 'https://www.v2ex.com'
+    }
+
+    v2ex_session = requests.Session()
+    # find once
+    once_value = search_page(v2ex_session, headers, login_url, 'name', 'once')['value']
+    post_info = {
+	'u' : username,
+	'p' : password,
+	'once' : once_value,
+	'next' : '/'
+    }
+    # login
+    resp = v2ex_session.post(login_url, data=post_info, headers=headers, verify=True)
+    short_url = search_page(v2ex_session, headers, mission_url, 'class', 'super normal button')['onclick']
+    first_quote = short_url.find("'")
+    last_quote = short_url.find("'", first_quote+1) #str.find(str, beg=0 end=len(string))
+    final_url = home_page + short_url[first_quote+1:last_quote]
+    logging.info(final_url)
+    # perform checkin
+    page = v2ex_session.get(final_url, headers=headers, verify=True).content
+    ok = search_page(v2ex_session, headers, mission_url, 'class', 'fa fa-ok-sign')
+
+def search_page(http, headers, url, tag, name):
+    page = http.get(url, headers=headers,verify=True).text
+    soup = BeautifulSoup(page, 'lxml')
+    soup_result = soup.find(attrs = {tag:name})
+    return soup_result
 
 def _config_log(echo, filename=None):
     #http_client.HTTPConnection.debuglevel = 1
@@ -94,3 +134,4 @@ def _config_log(echo, filename=None):
 _config_log(True, '/tmp/checkin.log')
 checkin_netease(cfg['netease']['music_u'], cfg['netease']['csrf'])
 checkin_zimuzu(cfg['zimuzu']['username'], cfg['zimuzu']['password'])
+checkin_v2ex(cfg['v2ex']['username'], cfg['v2ex']['password'])
