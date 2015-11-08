e28eff66f067436ee7b105cd27bb79e9aab79b5d
Dongsheng Cai <d@tux.im>
2015-11-08 14:01:59 +1100

    Clean up logging and add user-agent

--- checkin.py
+++ checkin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -- coding: utf-8 --
 
-import logging
+import logging.config
 import requests
 import time
 import configparser
@@ -14,10 +14,17 @@ except ImportError:
     # Python 2
     import httplib as http_client
 
+UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
+
 cfg = configparser.ConfigParser()
 dirname=os.path.dirname(os.path.realpath(sys.argv[0]))
 cfg.read("{}/credentials.ini".format(dirname))
 
+loggingconfigfile="{}/logging.ini".format(dirname)
+if os.path.isfile(loggingconfigfile):
+    logging.config.fileConfig(loggingconfigfile)
+
+l = logging.getLogger('checkin')
 
 def checkin_netease(music_u, csrf):
     TYPE_WEBPC = 1
@@ -27,7 +34,10 @@ def checkin_netease(music_u, csrf):
         '__remember_me': 'true',
         '__csrf': csrf
     }
-    headers = {'Referer': 'http://music.163.com/'}
+    headers = {
+        'User-Agent' : UA,
+        'Referer': 'http://music.163.com/'
+    }
     # web & pc
     url = 'http://music.163.com/api/point/dailyTask?type={}'.format(TYPE_WEBPC)
     response = requests.post(url, cookies=cookies, headers=headers)
@@ -42,11 +52,11 @@ def checkin_netease(music_u, csrf):
 def log_from_code(response_json, platform):
     code = response_json['code']
     if code == -2:
-        logging.info('{} failed. you already checked in today.'.format(platform))
+        l.info('{} failed. you already checked in today.'.format(platform))
     elif code == 200:
-        logging.info('{} ok. +{} points'.format(platform, response_json['point']))
+        l.info('{} ok. +{} points'.format(platform, response_json['point']))
     else:
-        logging.info('{} failed. {}({})'.format(platform, code, response_json['msg']))
+        l.info('{} failed. {}({})'.format(platform, code, response_json['msg']))
 
 def checkin_zimuzu(username, password):
     backurl = 'http://www.zimuzu.tv/user/sign'
@@ -56,14 +66,17 @@ def checkin_zimuzu(username, password):
         'remember': 1,
         'url_back': backurl
     }
-    headers = {'Referer': 'http://www.zimuzu.tv'}
+    headers = {
+        'User-Agent' : UA,
+        'Referer': 'http://www.zimuzu.tv'
+    }
     r = requests.post("http://www.zimuzu.tv/User/Login/ajaxLogin", data = data, headers=headers)
     o = r.json()
-    #logging.info(o['info'])
+    #l.info(o['info'])
 
     url = "http://www.zimuzu.tv/user/login/getCurUserTopInfo"
     r1 = requests.get(url, cookies=r.cookies, headers=headers)
-    #logging.info(r1.json())
+    #l.info(r1.json())
 
     # open checkin page and wait 16 seconds
     r2 = requests.get(backurl, cookies=r.cookies, headers=headers)
@@ -73,13 +86,12 @@ def checkin_zimuzu(username, password):
     url = "http://www.zimuzu.tv/user/sign/dosign"
     r3 = requests.get(url, cookies=r.cookies, headers=headers)
     o = r3.json()
-    logging.info(r3.content)
+    l.info(r3.content)
 
 def checkin_v2ex(username, password):
     login_url = 'https://v2ex.com/signin'
     home_page = 'https://www.v2ex.com'
     mission_url = 'https://www.v2ex.com/mission/daily'
-    UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
     headers = {
         'User-Agent' : UA,
         'Host' : "www.v2ex.com",
@@ -102,7 +114,7 @@ def checkin_v2ex(username, password):
     first_quote = short_url.find("'")
     last_quote = short_url.find("'", first_quote+1) #str.find(str, beg=0 end=len(string))
     final_url = home_page + short_url[first_quote+1:last_quote]
-    logging.info(final_url)
+    l.info(final_url)
     # perform checkin
     page = v2ex_session.get(final_url, headers=headers, verify=True).content
     ok = search_page(v2ex_session, headers, mission_url, 'class', 'fa fa-ok-sign')
@@ -113,25 +125,27 @@ def search_page(http, headers, url, tag, name):
     soup_result = soup.find(attrs = {tag:name})
     return soup_result
 
-def _config_log(echo, filename=None):
-    #http_client.HTTPConnection.debuglevel = 1
-    urllib3_logger = logging.getLogger('urllib3')
-    urllib3_logger.setLevel(logging.DEBUG)
-    requests_log = logging.getLogger("requests.packages.urllib3")
-    requests_log.setLevel(logging.DEBUG)
-    if echo:
-        if filename:
-            logging.basicConfig(level=logging.INFO,
-                                format='%(asctime)s %(message)s',
-                                datefmt='[%Y-%m-%d %H:%M:%S]',
-                                filename=filename,
-                                filemode='w')
-        else:
-            logging.basicConfig(level=logging.INFO,
-                                format='%(asctime)s %(message)s',
-                                datefmt='[%Y-%m-%d %H:%M:%S]')
+def checkin_smzdm(email, password):
+    BASE_URL = 'http://www.smzdm.com'
+    LOGIN_URL = BASE_URL + '/user/login/jsonp_check'
+    LOGIN_URL = 'https://zhiyou.smzdm.com/user/login/ajax_check'
+    CHECKIN_URL = BASE_URL + '/user/qiandao/jsonp_checkin'
+    headers = {
+        'User-Agent' : UA,
+        'Referer': BASE_URL
+    }
+    params = {
+        'user_login': email,
+        'user_pass': password,
+    }
+    l.info(headers)
+    l.info(params)
+    http = requests.Session()
+    r = http.get(BASE_URL, headers=headers)
+    r1 = http.get(LOGIN_URL, params=params, headers=headers)
+    r2 = http.get(CHECKIN_URL, headers=headers)
 
-_config_log(True, '/tmp/checkin.log')
 checkin_netease(cfg['netease']['music_u'], cfg['netease']['csrf'])
 checkin_zimuzu(cfg['zimuzu']['username'], cfg['zimuzu']['password'])
 checkin_v2ex(cfg['v2ex']['username'], cfg['v2ex']['password'])
+#checkin_smzdm(cfg['smzdm']['email'], cfg['smzdm']['password'])
