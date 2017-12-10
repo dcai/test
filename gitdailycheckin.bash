393dfe5d7a811f425ada818fd123622108893fce
Dongsheng Cai <d@tux.im>
2017-12-10 12:21:40 +1100

    Update gitdailycheckin.bash

--- gitdailycheckin.bash
+++ gitdailycheckin.bash
@@ -3,7 +3,7 @@
 cd /home/dcai/Dropbox/src/test/
 git stash -q
 git pull -r -q
-TT=`date +"%Y-%m-%d"`
+TT=$(date +"%Y-%m-%d")
 date >> log.txt
 git commit -a -m "$TT checkin" &> /dev/null
 git push origin master -u &> /dev/null
