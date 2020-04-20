92d4bfe1bdc4ef60db0a030cf8a3b9138834f317
Dongsheng Cai <d@tux.im>
2020-04-20 23:08:38 +1000

    gitdailycheckin.bash

--- gitdailycheckin.bash
+++ gitdailycheckin.bash
@@ -1,9 +1,9 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 cd /home/dcai/Dropbox/src/test/
 git stash -q
 git pull -r -q
 TT=$(date +"%Y-%m-%d")
-date >> log.txt
-git commit -a -m "$TT checkin" &> /dev/null
-git push origin master -u &> /dev/null
+date >>log.txt
+git commit -a -m "$TT checkin" &>/dev/null
+git push origin master -u &>/dev/null
