3478b0dd0d28dcf1eb95ad8f4cc96212dc7f78bc
dcai <heydcai@gmail.com>
2024-05-24 14:19:01 +1000

    test.js and biome.jsonc

--- test.js
+++ test.js
@@ -18,41 +18,41 @@ hello("dcai");
  */
 
 function toArray(l) {
-    var arr = [l.val];
-    while (l.next !== null) {
-        l = l.next;
-        arr.push(l.val);
-    }
-    return arr;
+  var arr = [l.val];
+  while (l.next !== null) {
+    l = l.next;
+    arr.push(l.val);
+  }
+  return arr;
 }
 const toLinkList = (arr) =>
-    arr.reverse().reduce((next, val) => {
-        return { val, next };
-    }, null);
+  arr.reverse().reduce((next, val) => {
+    return { val, next };
+  }, null);
 
 const toNum = (v) => {
-    if (v === undefined || v === null) return 0;
-    return v;
+  if (v === undefined || v === null) return 0;
+  return v;
 };
 
 var addTwoNumbers = function (li1, li2) {
-    var target = [];
-    l1 = toArray(li1);
-    l2 = toArray(li2);
-    var size = l1.length > l2.length ? l1.length : l2.length;
-    var addOne = 0;
-    for (var i = 0; i < size; i++) {
-        var sum = toNum(l1[i]) + toNum(l2[i]) + addOne;
-        target.push(sum < 10 ? sum : sum - 10);
-        addOne = sum - 10 >= 0 ? 1 : 0;
-    }
-    if (addOne !== 0) {
-        target.push(1);
-    }
-    return toLinkList(target);
+  var target = [];
+  l1 = toArray(li1);
+  l2 = toArray(li2);
+  var size = l1.length > l2.length ? l1.length : l2.length;
+  var addOne = 0;
+  for (var i = 0; i < size; i++) {
+    var sum = toNum(l1[i]) + toNum(l2[i]) + addOne;
+    target.push(sum < 10 ? sum : sum - 10);
+    addOne = sum - 10 >= 0 ? 1 : 0;
+  }
+  if (addOne !== 0) {
+    target.push(1);
+  }
+  return toLinkList(target);
 };
 
 const result = addTwoNumbers(
-    toLinkList([9, 9, 9, 9, 9, 9, 9]),
-    toLinkList([9, 9, 9, 9]),
+  toLinkList([9, 9, 9, 9, 9, 9, 9]),
+  toLinkList([9, 9, 9, 9]),
 );
