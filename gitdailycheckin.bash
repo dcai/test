c23047a8b833db31d3b52b11aeb04ad2233b0a55
Dongsheng Cai <d@tux.im>
2025-08-15 10:15:06 +1000

    fix: gitdailycheckin.bash

--- gitdailycheckin.bash
+++ gitdailycheckin.bash
@@ -1,10 +1,18 @@
 #!/usr/bin/bash
 
+log=/tmp/gitcheckin.log
+
 DIR=$(dirname "$0")
 cd "$DIR" || exit 1
 git stash -q
 git pull -r -q
 TT=$(date +"%Y-%m-%d")
 date >>logs/log.txt
-git commit -am "$TT checkin" &>/dev/null
-git push origin master -u &>/dev/null
+if [[ -n "$MONITOR_URL" ]]; then
+  curl "$MONITOR_URL?state=run"
+fi
+git commit -am "$TT checkin" &>"$log"
+git push origin master -u &>"$log"
+if [[ -n "$MONITOR_URL" ]]; then
+  curl "$MONITOR_URL?state=complete"
+fi
